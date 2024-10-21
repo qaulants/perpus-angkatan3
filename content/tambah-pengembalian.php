e4524476547710bbf117550577984d355268f3cf
qaulants <qaulantsaqila75@gmail.com>
2024-10-21 15:10:22 +0700

    update pengembalian

--- content/tambah-pengembalian.php
+++ content/tambah-pengembalian.php
@@ -1,18 +1,19 @@
 <?php
 if (isset($_POST['simpan'])) {
-    $no_peminjaman = $_POST['no_peminjaman'];
-    $status = $_POST['status'];
-    $denda = $_POST['tgl_peminjaman'];
-    $id_buku = $_POST['id_buku'];
-    $status = "Di Pinjam";
-    $insert = mysqli_query($koneksi, "INSERT INTO peminjaman (no_peminjaman, id_anggota, tgl_peminjaman, tgl_pengembalian, status) VALUES ('$no_peminjaman', '$id_anggota', '$tgl_peminjaman', '$tgl_pengembalian', '$status')");
-    $id_peminjaman = mysqli_insert_id($koneksi);
+    $id_peminjaman = $_POST['id_peminjaman'];
+    $queryPeminjam = mysqli_query($koneksi, "SELECT id, no_peminjaman FROM peminjaman WHERE no_peminjaman='$id_peminjaman'");
 
-    foreach ($id_buku as $key => $buku) {
-        $id_buku = $_POST['id_buku'][$key];
-        $inserDetail = mysqli_query($koneksi, "INSERT INTO detail_peminjaman(id_peminjaman, id_buku) VALUES ('$id_peminjaman', '$id_buku')");
-    }
-    header("location:?pg=peminjaman&tambah=berhasil");
+    $rowPeminjaman = mysqlI_fetch_assoc($queryPeminjam);
+    $id_peminjaman = $rowPeminjaman["id"];
+    $denda = $_POST['denda'];
+   if ($denda == 0) {
+        $status = 0;
+   }else{
+        $status = 1;
+   }
+    $insert = mysqli_query($koneksi, "INSERT INTO pengembalian (id_peminjaman, status, denda) VALUES ('$id_peminjaman', '$status', '$denda')");
+    $updatePeminjaman = mysqli_query($koneksi, "UPDATE peminjaman SET status = 'Di Kembalikan' WHERE id='$id_peminjaman'");
+    header("location:?pg=pengembalian&tambah=berhasil");
 }
 
 if (isset($_GET['delete'])) {
@@ -28,11 +29,8 @@ $rowPeminjam = mysqli_fetch_assoc($queryPeminjaman);
 
 $queryDetailPinjam = mysqli_query($koneksi, "SELECT buku.nama_buku, detail_peminjaman.* FROM detail_peminjaman LEFT JOIN buku ON buku.id = detail_peminjaman.id_buku WHERE id_peminjaman = '$id'");
 
-
 $queryBuku = mysqli_query($koneksi, "SELECT * FROM buku");
 $queryKodePinjam = mysqli_query($koneksi, "SELECT * FROM peminjaman WHERE status='Di Pinjam'");
-
-
 
 $queryAnggota = mysqli_query($koneksi, "SELECT * FROM anggota");
 ?>
@@ -68,19 +66,22 @@ $queryAnggota = mysqli_query($koneksi, "SELECT * FROM anggota");
                                                 <input class="form-control" type="text" readonly id="no_pinjam">
                                             </div>
                                             <div class="mb-3">
-                                                <label class="form-label" for="">Nama Anggota</label>
-                                                <input class="form-control" type="text" readonly id="nama_anggota">
+                                                <label class="form-label" for="">Tanggal Peminjaman</label>
+                                                <input class="form-control" type="text" readonly id="tgl_peminjaman" value="">
                                             </div>
-                                          
+                                            <div class="mb-3">
+                                                <label class="form-label" for="">Denda</label>
+                                                <input class="form-control" type="text" readonly id="denda" name="denda" value="">
+                                            </div>
                                         </div>
                                         <div class="col-sm-6">
                                             <div class="mb-3">
-                                                <label class="form-label" for="">Tanggal Peminjaman</label>
+                                                <label class="form-label" for="">Nama Anggota</label>
                                                 <input class="form-control" type="text" readonly id="nama_anggota">
                                             </div>
                                             <div class="mb-3">
                                                 <label class="form-label" for="">Tanggal Pengembalian</label>
-                                                <input class="form-control" type="text" readonly id="nama_anggota">
+                                                <input class="form-control" type="text" readonly id="tgl_pengembalian">
                                             </div>
                                         </div>
                                             
@@ -90,32 +91,12 @@ $queryAnggota = mysqli_query($koneksi, "SELECT * FROM anggota");
                         </div>
                     </div>
                    
-                    <!-- ini table dari query dengan php -->
-                    <?php if (!empty($_GET['detail'])): ?>
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>No </th>
-                                    <th>Nama Buku</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php $no = 1;
-                                while ($rowDetailPeminjaman = mysqli_fetch_assoc($queryDetailPinjam)): ?>
-                                    <tr>
-                                        <td><?php echo $no++ ?></td>
-                                        <td><?php echo $rowDetailPeminjaman['nama_buku'] ?></td>
-                                    </tr>
-                                <?php endwhile ?>
-                            </tbody>
-                        </table>
-
-                    <?php else: ?>
-                        <table id="table" class="table table-bordered">
+              
+                        <table id="table-pengembalian" class="table table-bordered">
                             <thead>
                                 <tr>
                                     <th>Nama Buku</th>
-                                    <th>Aksi</th>
+                      
                                 </tr>
                             </thead>
                             <tbody class="table-row">
@@ -125,7 +106,7 @@ $queryAnggota = mysqli_query($koneksi, "SELECT * FROM anggota");
                         <div class="mt-3">
                             <button type="submit" name="simpan" class="btn btn-secondary">Simpan</button>
                         </div>
-                    <?php endif ?>
+             
 
 
                 </form>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 if (empty($_SESSION['NAMA'])) {
     header("location:login.php?access=failed");
 }
- include 'koneksi.php';
+include 'koneksi.php';
 
 ?>
 <!DOCTYPE html>
@@ -14,6 +14,7 @@ if (empty($_SESSION['NAMA'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="bootstrap-5.3.3/dist/css//bootstrap.min.css">
+
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel='stylesheet' href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css'>
@@ -41,25 +42,50 @@ if (empty($_SESSION['NAMA'])) {
     <?php include 'layout/footer.php'; ?>
     <!-- <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script> -->
     <script src="bootstrap-5.3.3/dist/js/jquery-3.7.1.min.js"></script>
+    <script src="bootstrap-5.3.3/dist/js/moment.js"></script>
     <script src="bootstrap-5.3.3/dist/js/bootstrap.bundle.js"></script>
     <script src="app.js"></script>
     <script>
-        $('#id_peminjaman').change(function() {//sama kaya $('#id_peminjaman')
+        $('#id_peminjaman').change(function() { //sama kaya $('#id_peminjaman')
             let no_peminjaman = $(this).find('option:selected').val();
-            console.log(no_peminjaman)
+            let tbody = $('tbody'),
+                newRow = "";
             $.ajax({
-                url:"ajax/getPeminjam.php?no_peminjaman=" + no_peminjaman,
+                url: "ajax/getPeminjam.php?no_peminjaman=" + no_peminjaman,
                 type: "get",
                 dataType: "json",
-                success:function(res){
+                success: function(res) {
                     $('#no_pinjam').val(res.data.no_peminjaman);
-                    $('#tgl_piminjaman').val(res.data.tgl_peminjaman);
+                    $('#tgl_peminjaman').val(res.data.tgl_peminjaman);
                     $('#tgl_pengembalian').val(res.data.tgl_pengembalian);
                     $('#nama_anggota').val(res.data.nama_anggota);
+                    //console.log(res)
+
+                    let tanggal_kembali = new moment(res.data.tgl_pengembalian);
+
+                    let currentDate = new Date().toJSON().slice(0, 10);
+                    console.log(currentDate);
+
+                    let tanggal_di_kembalikan = new moment(currentDate)
+                    let selisih = tanggal_di_kembalikan.diff(tanggal_kembali, "days");
+
+                    if (selisih < 0) {
+                        selisih = 0;
+                    }
+                    
+                    let biaya_denda = 10000;
+                    let totalDenda = selisih * biaya_denda;
+                    $('#denda').val(totalDenda);
+
+                    $.each(res.detail_peminjaman, function(key, val) {
+                        newRow += "<tr>";
+                        newRow += "<td>" + val.nama_buku + "</td>";
+                        newRow += "</tr>";
+                    });
+                    tbody.html(newRow);
                 }
             });
         });
-
     </script>
 </body>
 
